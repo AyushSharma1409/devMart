acb70a2baaa012eb08a97f5d5d6958e5cb94e609
AyushSharma1409 <108172281+AyushSharma1409@users.noreply.github.com>
2022-07-27 02:52:29 +0530

    my first commit reactwebsite

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <>
+        <footer className="w-100 bg-light text-center">
+        <p>ⓒ 2022 AyushSharma. All Rights Reserved | Terms and Conditions</p>
+        </footer>
+        </>
+    );
+};
+
+export default Footer;
