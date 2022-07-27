1a7de8a464f22577ac6e141154eecc6f6de00480
AyushSharma1409 <108172281+AyushSharma1409@users.noreply.github.com>
2022-07-27 09:16:52 +0530

    Updates

---
+++ precache-manifest.a1e4bc41a5eefde2f8fd633b130d55d1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "91309446f573131a5352",
+    "url": "/devvMart/static/css/main.772d337e.chunk.css"
+  },
+  {
+    "revision": "91309446f573131a5352",
+    "url": "/devvMart/static/js/main.91309446.chunk.js"
+  },
+  {
+    "revision": "5be94f57fbeabde55105",
+    "url": "/devvMart/static/css/1.60bc9028.chunk.css"
+  },
+  {
+    "revision": "5be94f57fbeabde55105",
+    "url": "/devvMart/static/js/1.5be94f57.chunk.js"
+  },
+  {
+    "revision": "d96e7b308ba9b1ba9b2e",
+    "url": "/devvMart/static/js/runtime~main.d96e7b30.js"
+  },
+  {
+    "revision": "d9963afdd4c2aad0d1b47e33f71a6a73",
+    "url": "/devvMart/static/media/chngeee.d9963afd.jpg"
+  },
+  {
+    "revision": "e6dc64beb6234fb40704937aaeebbb5b",
+    "url": "/devvMart/static/media/webd.e6dc64be.jpg"
+  },
+  {
+    "revision": "c67b4b5c04f426c7916b32e59c2091f8",
+    "url": "/devvMart/static/media/appe.c67b4b5c.jpg"
+  },
+  {
+    "revision": "5aa8f9afb3e5ef103569ed73ccafa316",
+    "url": "/devvMart/static/media/swd.5aa8f9af.jpg"
+  },
+  {
+    "revision": "b906b142b42ceffaf16990b4ced2a830",
+    "url": "/devvMart/static/media/digite.b906b142.jpg"
+  },
+  {
+    "revision": "9df84ad34caad422c4ba20b169c0a02d",
+    "url": "/devvMart/static/media/andr.9df84ad3.jpg"
+  },
+  {
+    "revision": "f65be0a19046394a386913361729137c",
+    "url": "/devvMart/static/media/mrket.f65be0a1.jpg"
+  },
+  {
+    "revision": "4c85888f5148e60676765e31a0f205c1",
+    "url": "/devvMart/static/media/unnamed.4c85888f.jpg"
+  },
+  {
+    "revision": "e7e775642f6de7f40637bb2b227e419d",
+    "url": "/devvMart/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/devvMart/precache-manifest.a1e4bc41a5eefde2f8fd633b130d55d1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/devvMart/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
